3a4295b9d16c835434e59ac344dd47eacc16cf80
sergiovivart <sevivarte@gmail.com>
2025-12-20 04:18:10 +0100

    eliminada migracion inecesaria

--- inmobiliaria/app/Models/Cities.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-// Backwards compatibility class: alias plural `Cities` to singular `Ciudad`
-class Cities extends Ciudad
-{
-    // left intentionally empty; use `Ciudad` methods/properties
-}
